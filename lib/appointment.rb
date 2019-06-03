288e4e112d037582795f2da66085967e5b5f273a
Margot Konig <margot.konig@blackrock.com>
2019-06-03 03:56:30 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -2,10 +2,10 @@ class Appointment
   attr_accessor :doctor, :date, :patient
   
   @@all = []
-  def initialize(name, artist, genre)
-    @name = name
-    @artist = artist
-    @genre = genre 
+  def initialize(patient, doctor, date)
+    @patient = patient
+    @doctor = doctor
+    @date = date 
     @@all << self
   end
   
