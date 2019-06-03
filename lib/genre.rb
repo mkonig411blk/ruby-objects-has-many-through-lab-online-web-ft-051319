a793f4c67434cd1b31cee89c9eb1fd130916e75f
Margot Konig <margot.konig@blackrock.com>
2019-06-03 03:46:34 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -10,5 +10,15 @@ class Genre
   end
   
   def songs 
+    Song.all.select do |song|
+      song.genre == self 
+    end
+  end
+  
+  def artists 
+    songs.map do |song|
+      song.artist
+    end
+  end
   
 end
