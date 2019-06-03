5735a358b02e51bbd83cf2b1e3d16c78e8260e4b
Margot Konig <margot.konig@blackrock.com>
2019-06-03 03:42:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,6 +7,17 @@ class Artist
     @all << self
   end 
   
+  def self.all 
+    @@all 
+  end 
+  
   def new_song(name, genre)
-    
+    new_song = Song.new(name, genre)
+    new_song.artist = self 
+  end
+  
+  def songs 
+    Song.all.detect do |song|
+      
+  
 end
